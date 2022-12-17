7286ee6b8c4b5a09bb80638acdd61375d03d52b8
Мороз <morozov.nva@yandex.ru>
2022-12-18 01:24:32 +0300

    commit 2

--- src/num4_RomanToInteger/Solution.java
+++ src/num4_RomanToInteger/Solution.java
@@ -14,83 +14,45 @@ public class Solution {
         int num1 = 0;
         int num10 = 0;
         int num100 = 0;
-        index = 0;
+        int num1000 = 0;
+        index = 1;
 
         char[] chars = new char[s.length()];
         for (int i = 0; i < s.length(); i++) {
             chars[i] = s.charAt(s.length() - i - 1);
         }
 
-        if (chars[0] == 'V') {
-            if (s.length() > 1) {
-                if (chars[1] == 'I') {
-                    num1 = 4;
-                    index += 3;
-                } else {
-                    num1 = 5;
-                    index += 2;
-                }
-            } else {
-                num1 = 5;
-                index++;
-            }
-        } else if (chars[0] == 'X') {
-            if (s.length() > 1) {
-                if (chars[1] == 'I') {
-                    num1 = 9;
-                    index += 3;
-                } else {
-                    num1 = 0;
-                    index++;
-                }
-            } else {
-                num1 = 0;
-                index++;
-            }
-        } else {
-            for (int i = 0; i < s.length(); i++) {
-                if (chars[i] == 'I') {
-                    num1++;
-                    index++;
-                } else if (chars[i] == 'V') {
-                    num1 += 5;
-                    index += 2;
-                    break;
-                } else {
-                    index++;
-                    break;
-                }
-            }
-        }
+        num1 = method(s, chars, 'I', 'V', 'X');
 
         if (index - 1 < s.length()) num10 = method(s, chars, 'X', 'L', 'C') * 10;
-        if (index - 1 < s.length()) num100 = method(s, chars, 'Ñ', 'D', 'M') * 100;
 
-        System.out.print("index = " + index + ", number = ");
-        return num1 + num10;
+        if (index - 1 < s.length()) num100 = method(s, chars, 'C', 'D', 'M') * 100;
+        if (index - 1 < s.length()) num1000 = method(s, chars, 'M', 'O', 'O') * 1000;
+
+        return num1 + num10 + num100 + num1000;
     }
 
     public int method(String s, char[] chars, char I, char V, char X) {
         index--;
         int num1 = 0;
         if (chars[index] == V) {
-            if (s.length() > 1) {
+            if (s.length() > index + 1) {
                 if (chars[index + 1] == I) {
                     num1 = 4;
-                    index+=3;
+                    index += 3;
                 } else {
                     num1 = 5;
-                    index+=2;
+                    index += 2;
                 }
             } else {
                 num1 = 5;
                 index++;
             }
         } else if (chars[index] == X) {
-            if (s.length() > 1) {
+            if (s.length() > index + 1) {
                 if (chars[index + 1] == I) {
                     num1 = 9;
-                    index+=3;
+                    index += 3;
                 } else {
                     num1 = 0;
                     index++;
@@ -117,6 +79,41 @@ public class Solution {
         return num1;
     }
 
+    public int method2(String s) {
+        int pre = 0, cur = 0, result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case 'M':
+                    cur = 1000;
+                    break;
+                case 'D':
+                    cur = 500;
+                    break;
+                case 'C':
+                    cur = 100;
+                    break;
+                case 'L':
+                    cur = 50;
+                    break;
+                case 'X':
+                    cur = 10;
+                    break;
+                case 'V':
+                    cur = 5;
+                    break;
+                case 'I':
+                    cur = 1;
+                    break;
+            }
+
+            if (cur > pre) {
+                result = result - pre - pre + cur;
+            } else result += cur;
+            pre = cur;
+        }
+        return result;
+    }
+
     public static void main(String[] args) throws IOException {
         File reader = new File("input.txt");
         reader.createNewFile();
@@ -133,7 +130,9 @@ public class Solution {
         int i = 0;
         while (i < command2.size()) {
             System.out.print(command2.get(i) + " -> ");
-            System.out.println(solution.romanToInt(command2.get(i++)));
+            System.out.println(" number=" + solution.romanToInt(command2.get(i))
+                    + " | " + solution.method2(command2.get(i++)));
+
         }
 
 
